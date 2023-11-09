f49f9a7b6835581fee34cad8547bd340d5799928
Maryan Pasichniak <maryan.pasichniak@ekar.me>
2023-11-09 09:38:50 +0200

    code cleaning

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ import {
     getAssociatedTokenAddress,
     createAssociatedTokenAccountInstruction,
     createMintToInstruction,
-    createBurnCheckedInstruction
+    createBurnCheckedInstruction,
+    createMintToCheckedInstruction
 } from "@solana/spl-token"
 import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata"
 import {
@@ -35,7 +36,9 @@ import bs58 from "bs58"
 function App() {
     const [name, setName] = useState("");
     const [code, setCode] = useState("");
-    ///
+    const [amount, setAmount] = useState("");
+    const [tokenMint, setTokenMint] = useState("");
+    // const [usersPrivateKey, setUsersPrivateKey] = useState("");
 
     const endpoint =
         "https://aged-few-gas.solana-devnet.quiknode.pro/709859f8e8b4d80991023ddd417b320d0e139e84/" //Replace with your RPC Endpoint
@@ -46,7 +49,7 @@ function App() {
 
     const numDecimals = 6
 
-    const userWallet = Keypair.fromSecretKey(bs58.decode(secretKey)) //Keypair.fromSecretKey(new Uint8Array(secret));
+    const userWallet = Keypair.fromSecretKey(bs58.decode(secretKey))
     const metaplex = Metaplex.make(solanaConnection)
         .use(keypairIdentity(userWallet))
         .use(
@@ -62,14 +65,14 @@ function App() {
         numberTokens: 100
     }
     const MY_TOKEN_METADATA = {
-        name: "Marikk Token",
+        name: "My New Token",
         symbol: "MkkT",
-        description: "This is my first tok!",
+        description: "This is my first token!",
         image:
             "https://images.ctfassets.net/q5ulk4bp65r7/45uk7WZNNBGCHOwlNaGCT4/a4c8897e2cae08e4f42bf56ca6e3ba4b/solona.png" //add public URL to image you'd like to use
     }
     const ON_CHAIN_METADATA = {
-        name: "Marikk Token",
+        name: "My New Token",
         symbol: "MkkT",
         uri: "TO_UPDATE_LATER",
         sellerFeeBasisPoints: 0,
@@ -105,131 +108,73 @@ function App() {
             })
         }
 
-    const createNewMintTransaction = async (
-        connection,
-        payer,
-        mintKeypair,
-        destinationWallet,
-        mintAuthority,
-        freezeAuthority
-    ) => {
-        //Get the minimum lamport balance to create a new account and avoid rent payments
-        const requiredBalance = await getMinimumBalanceForRentExemptMint(connection)
-        //metadata account associated with mint
-        const metadataPDA = await metaplex
-            .nfts()
-            .pdas()
-            .metadata({ mint: mintKeypair.publicKey })
-        //get associated token account of your wallet
-        const tokenATA = await getAssociatedTokenAddress(
-            mintKeypair.publicKey,
-            destinationWallet
-        )
-
-        const createNewTokenTransaction = new Transaction().add(
-            SystemProgram.createAccount({
-                fromPubkey: payer.publicKey,
-                newAccountPubkey: mintKeypair.publicKey,
-                space: MINT_SIZE,
-                lamports: requiredBalance,
-                programId: TOKEN_PROGRAM_ID
-            }),
-            createInitializeMintInstruction(
-                //Mint Address
-                mintKeypair.publicKey, //Number of Decimals of New mint
-                MINT_CONFIG.numDecimals, //Mint Authority
-                mintAuthority, //Freeze Authority
-                freezeAuthority,
-                TOKEN_PROGRAM_ID
-            ),
-            createAssociatedTokenAccountInstruction(
-                //Payer
-                payer.publicKey, //Associated token account
-                tokenATA, //token owner
-                payer.publicKey, //Mint
-                mintKeypair.publicKey
-            ),
-            createMintToInstruction(
-                //Mint
-                mintKeypair.publicKey, //Destination Token Account
-                tokenATA, //Authority
-                mintAuthority, //number of tokens
-                MINT_CONFIG.numberTokens * Math.pow(10, MINT_CONFIG.numDecimals)
-            ),
-            createCreateMetadataAccountV3Instruction(
-                {
-                    metadata: metadataPDA,
-                    mint: mintKeypair.publicKey,
-                    mintAuthority: mintAuthority,
-                    payer: payer.publicKey,
-                    updateAuthority: mintAuthority
-                },
-                {
-                    createMetadataAccountArgsV3: {
-                        data: ON_CHAIN_METADATA,
-                        isMutable: true,
-                        collectionDetails: null
+        async function createNewMintTransaction(connection, payer, mintKeypair,
+                                                destinationWallet, mintAuthority, freezeAuthority){
+            //Get the minimum lamport balance to create a new account and avoid rent payments
+            console.log(`---STEP 2.1: Get the minimum lamport balance to create a new account and avoid rent payments---`)
+            const requiredBalance = await getMinimumBalanceForRentExemptMint(connection)
+            //metadata account associated with mint
+            const metadataPDA = await metaplex
+                .nfts()
+                .pdas()
+                .metadata({ mint: mintKeypair.publicKey })
+            //get associated token account of your wallet
+            const tokenATA = await getAssociatedTokenAddress(
+                mintKeypair.publicKey,
+                destinationWallet
+            )
+            console.log(`---STEP 2.2: createNewTokenTransaction---`)
+            const createNewTokenTransaction = new Transaction().add(
+                SystemProgram.createAccount({
+                    fromPubkey: payer.publicKey,
+                    newAccountPubkey: mintKeypair.publicKey,
+                    space: MINT_SIZE,
+                    lamports: requiredBalance,
+                    programId: TOKEN_PROGRAM_ID
+                }),
+                createInitializeMintInstruction(
+                    //Mint Address
+                    mintKeypair.publicKey, //Number of Decimals of New mint
+                    MINT_CONFIG.numDecimals, //Mint Authority
+                    mintAuthority, //Freeze Authority
+                    freezeAuthority,
+                    TOKEN_PROGRAM_ID
+                ),
+                createAssociatedTokenAccountInstruction(
+                    //Payer
+                    payer.publicKey, //Associated token account
+                    tokenATA, //token owner
+                    payer.publicKey, //Mint
+                    mintKeypair.publicKey
+                ),
+                // createMintToInstruction(
+                //     //Mint
+                //     mintKeypair.publicKey, //Destination Token Account
+                //     tokenATA, //Authority
+                //     mintAuthority, //number of tokens
+                //     MINT_CONFIG.numberTokens * Math.pow(10, MINT_CONFIG.numDecimals)
+                // ),
+                createCreateMetadataAccountV3Instruction(
+                    {
+                        metadata: metadataPDA,
+                        mint: mintKeypair.publicKey,
+                        mintAuthority: mintAuthority,
+                        payer: payer.publicKey,
+                        updateAuthority: mintAuthority
+                    },
+                    {
+                        createMetadataAccountArgsV3: {
+                            data: ON_CHAIN_METADATA,
+                            isMutable: true,
+                            collectionDetails: null
+                        }
                     }
-                }
-            )
-        )
-
-        return createNewTokenTransaction
-    }
-
-    const main = async () => {
-        try {
-            console.log(`---STEP 1: Uploading MetaData---`)
-            const userWallet = Keypair.fromSecretKey(bs58.decode(secretKey)) //Keypair.fromSecretKey(new Uint8Array(secret));
-            console.log("Userwallet", userWallet)
-            let metadataUri = await uploadMetadata(MY_TOKEN_METADATA)
-            console.log("metadataUri", metadataUri)
-
-            ON_CHAIN_METADATA.uri = metadataUri
-
-            console.log(`---STEP 2: Creating Mint Transaction---`)
-            let mintKeypair = Keypair.generate()
-            console.log(`New Mint Address: `, mintKeypair.publicKey.toString())
-
-            const newMintTransaction = await createNewMintTransaction(
-                solanaConnection,
-                userWallet,
-                mintKeypair,
-                userWallet.publicKey,
-                userWallet.publicKey,
-                userWallet.publicKey
+                )
             )
 
-            console.log(`---STEP 3: Executing Mint Transaction---`)
-            let {
-                lastValidBlockHeight,
-                blockhash
-            } = await solanaConnection.getLatestBlockhash("finalized")
-            newMintTransaction.recentBlockhash = blockhash
-            newMintTransaction.lastValidBlockHeight = lastValidBlockHeight
-            newMintTransaction.feePayer = userWallet.publicKey
-            const transactionId = await sendAndConfirmTransaction(
-                solanaConnection,
-                newMintTransaction,
-                [userWallet, mintKeypair]
-            )
-            console.log(`Transaction ID: `, transactionId)
-            console.log(
-                `Succesfully minted ${MINT_CONFIG.numberTokens} ${
-                    ON_CHAIN_METADATA.symbol
-                } to ${userWallet.publicKey.toString()}.`
-            )
-            console.log(
-                `View Transaction: https://explorer.solana.com/tx/${transactionId}?cluster=devnet`
-            )
-            console.log(
-                `View Token Mint: https://explorer.solana.com/address/${mintKeypair.publicKey.toString()}?cluster=devnet`
-            )
-        } catch (e) {
-            console.log("Error", e)
+            console.log(`---STEP 2.6: Final return---`)
+            return createNewTokenTransaction
         }
-
-    }
 
     async function burnTokens(
         wallet,
@@ -284,21 +229,131 @@ function App() {
     }
 
 
+
+    async function mintToken(
+        wallet,
+        tokenMint, // The mint associated with the token
+        amountToBurn // The number of tokens to burn
+    ) {
+        console.log(`Attempting to minToken2 ${amountToBurn} [${tokenMint}] tokens from Owner Wallet: ${wallet.publicKey.toString()}`);
+
+        // Step 1 - Fetch Associated Token Account Address
+        console.log(`Step 1 - Fetch Token Account`);
+        const account = await getAssociatedTokenAddress(new PublicKey(tokenMint), wallet.publicKey);
+        console.log(`    ✅ - Associated Token Account Address: ${account.toString()}`);
+
+        // Step 2 - Create Burn Instructions
+        console.log(`Step 2 - Create minToken2 Instructions`);
+        const burnIx = createMintToCheckedInstruction(
+            new PublicKey(tokenMint),
+            new PublicKey("3fj5pqcPKWYLzJsqB2STbyxvGM8hr98AFtpjGA5uwpX6"),
+            new PublicKey("GcbygsphsrzZZ7v3RvAmR6w7BZERHNi5ghGxcrNjmVj4"),
+            amountToBurn * (10**numDecimals),
+            numDecimals
+        );
+        console.log(`    ✅ - minToken2 Instruction Created`);
+
+        // Step 3 - Fetch Blockhash
+        console.log(`Step 3 - Fetch Blockhash`);
+        const { blockhash, lastValidBlockHeight } = await solanaConnection.getLatestBlockhash('finalized');
+        console.log(`    ✅ - Latest Blockhash: ${blockhash}`);
+
+        // Step 4 - Assemble Transaction
+        console.log(`Step 4 - Assemble Transaction`);
+        const messageV0 = new TransactionMessage({
+            payerKey: wallet.publicKey,
+            recentBlockhash: blockhash,
+            instructions: [burnIx]
+        }).compileToV0Message();
+        const transaction = new VersionedTransaction(messageV0);
+        transaction.sign([wallet]);
+        console.log(`    ✅ - Transaction Created and Signed`);
+
+        // Step 5 - Execute & Confirm Transaction
+        console.log(`Step 5 - Execute & Confirm Transaction`);
+        const txid = await solanaConnection.sendTransaction(transaction);
+        console.log("    ✅ - Transaction sent to network");
+        const confirmation = await solanaConnection.confirmTransaction({
+            signature: txid,
+            blockhash: blockhash,
+            lastValidBlockHeight: lastValidBlockHeight
+        });
+        if (confirmation.value.err) { throw new Error("❌ - Transaction not confirmed.") }
+        console.log('🔥 SUCCESSFUL minToken2!🔥', '\n', `https://explorer.solana.com/tx/${txid}?cluster=devnet`);
+    }
+
+    async function createToken() {
+        console.log(`---STEP 1: Uploading MetaData---`)
+        const userWallet = Keypair.fromSecretKey(bs58.decode(secretKey))
+        let metadataUri = await uploadMetadata(MY_TOKEN_METADATA)
+        ON_CHAIN_METADATA.uri = metadataUri
+
+        console.log(`---STEP 2: Creating Mint Transaction---`)
+        let mintKeypair = Keypair.generate()
+        console.log(`New Mint Address: `, mintKeypair.publicKey.toString())
+
+        const newMintTransaction = await createNewMintTransaction(
+            solanaConnection,
+            userWallet,
+            mintKeypair,
+            userWallet.publicKey,
+            userWallet.publicKey,
+            userWallet.publicKey
+        )
+
+        console.log(solanaConnection.description)
+
+        console.log(`---STEP 3: Executing Mint Transaction---`)
+        let {
+            lastValidBlockHeight,
+            blockhash
+        } = await solanaConnection.getLatestBlockhash("finalized")
+        newMintTransaction.recentBlockhash = blockhash
+        newMintTransaction.lastValidBlockHeight = lastValidBlockHeight
+        newMintTransaction.feePayer = userWallet.publicKey
+        const transactionId = await sendAndConfirmTransaction(
+            solanaConnection,
+            newMintTransaction,
+            [userWallet, mintKeypair]
+        )
+        console.log(`Transaction ID: `, transactionId)
+        console.log(
+            `Succesfully minted ${MINT_CONFIG.numberTokens} ${
+                ON_CHAIN_METADATA.symbol
+            } to ${userWallet.publicKey.toString()}.`
+        )
+        console.log(
+            `View Transaction: https://explorer.solana.com/tx/${transactionId}?cluster=devnet`
+        )
+        console.log(
+            `View Token Mint: https://explorer.solana.com/address/${mintKeypair.publicKey.toString()}?cluster=devnet`
+        )
+    }
+
+
     ///
   return (
     <div className="App">
       <h1>Demo Token Creation</h1>
+        <div>
+            {/*<div>*/}
+            {/*    <label htmlFor="Users Private Key">Enter Users Private Key:</label>*/}
+            {/*    <input value={usersPrivateKey} onChange={(e) => setUsersPrivateKey(e.target.value)}/>*/}
+            {/*</div>*/}
+            <div>
+                <label htmlFor="Token Mint">Enter TokenMint:</label>
+                <input value={tokenMint} onChange={(e) => setTokenMint(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="amount">Enter Amount:</label>
+                <input value={amount} onChange={(e) => setAmount(e.target.value)}/>
+            </div>
+            <button onClick={async () => await burnTokens(userWallet, tokenMint, amount)}>Burn</button>
+            <div style={{ width: '10px' }}></div> {/* Add a space element */}
+            <button onClick={async () => await mintToken(userWallet, tokenMint, amount)}>Mint</button>
+            <div style={{ width: '10px' }}></div> {/* Add a space element */}
 
-      <div>
-          <div>Name</div>
-          <input value={name} onChange={(e) => setName(e.target.value)}/>
-      </div>
-        <div>
-            <div>Code</div>
-            <input value={code} onChange={(e) => setCode(e.target.value)}/>
-        </div>
-        <div>
-            <button onClick={async () => await main()}>Create</button>
+            {/*<button onClick={async () => await createToken()}>Create_Token</button>*/}
         </div>
     </div>
   );
