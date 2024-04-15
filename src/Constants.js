490cf9473390c08dd9bcd2b30facdc03495b4b1c
Maryan Pasichniak <maryan.pasichniak@ekar.me>
2024-04-15 15:30:20 +0300

    port fix

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
 import {clusterApiUrl, Connection, Keypair} from "@solana/web3.js";
 import bs58 from "bs58";
 
-export const BASE_URL = 'http://13.212.56.212:3000';
+export const BASE_URL = 'http://13.212.56.212:300';
